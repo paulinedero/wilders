47c7bbf42500e1573e78191ecfc1cefa27d06a8e
Pauline De Ro <pauline.dero@gmail.com>
2021-03-18 15:17:35 +0100

    new repo initialized

---
+++ index.js
@@ -0,0 +1,7 @@
+const chalk = require('chalk');
+ 
+console.log(chalk.blue('Dumitru'));
+console.log(chalk.red('Xavier'));
+console.log(chalk.green('Leopold'));
+console.log(chalk.yellow('Sam'));
+console.log(chalk.blue('Mario'));
